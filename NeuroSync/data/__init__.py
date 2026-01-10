69bbc29acd4332d347418c25a7590a369fd415b3
Nikolay Valkanov <niki@valkanovi.com>
2026-01-11 01:49:12 +0200

    UPDATE: Added proper description to the purpose of this module

--- NeuroSync/data/__init__.py
+++ NeuroSync/data/__init__.py
@@ -1,3 +1,9 @@
+"""
+Data handling for NeuroSync.
+
+Provides word list loading, word list creation, and message generation.
+"""
+
 from NeuroSync.data.loaders import WordListLoader, load_word_list, create_word_list
 from NeuroSync.data.generators import MessageGenerator, generate_random_messages
 
