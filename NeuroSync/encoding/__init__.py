81f9d0853f52b4e9b234520cee0be11509d07936
Nikolay Valkanov <niki@valkanovi.com>
2026-01-12 01:04:38 +0200

    UPDATE: Added documentation for the purpose of this module

--- NeuroSync/encoding/__init__.py
+++ NeuroSync/encoding/__init__.py
@@ -1,3 +1,9 @@
+"""
+Bit encoding and decoding for NeuroSync.
+
+Handles conversion between text and binary representations.
+"""
+
 from NeuroSync.encoding.codec import text_to_bits, bits_to_text
 from NeuroSync.encoding.batch import text_to_bits_batch, bits_to_text_batch
 from NeuroSync.encoding.constants import (
