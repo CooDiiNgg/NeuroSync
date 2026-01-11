593cb24b77a273997b33bcfbbbe0225de3b88c1e
Nikolay Valkanov <niki@valkanovi.com>
2026-01-12 00:50:50 +0200

    UPDATE: Included all necessary things from the module

---
+++ NeuroSync/encoding/__init__.py
@@ -0,0 +1,19 @@
+from NeuroSync.encoding.codec import text_to_bits, bits_to_text
+from NeuroSync.encoding.batch import text_to_bits_batch, bits_to_text_batch
+from NeuroSync.encoding.constants import (
+    MESSAGE_LENGTH,
+    BITS_PER_CHAR,
+    BIT_LENGTH,
+    CHARSET,
+)
+
+__all__ = [
+    "text_to_bits",
+    "bits_to_text",
+    "text_to_bits_batch",
+    "bits_to_text_batch",
+    "MESSAGE_LENGTH",
+    "BITS_PER_CHAR",
+    "BIT_LENGTH",
+    "CHARSET",
+]
