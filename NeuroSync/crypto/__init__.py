8f924d215b02bba176b89d60a52dde814524cdab
Nikolay Valkanov <niki@valkanovi.com>
2026-01-13 18:50:16 +0200

    UPDATE: defined all functionalities of this module and exposed them

---
+++ NeuroSync/crypto/__init__.py
@@ -0,0 +1,9 @@
+from NeuroSync.crypto.operations import xor
+from NeuroSync.crypto.keys import KeyManager
+from NeuroSync.crypto.weights import WeightManager
+
+__all__ = [
+    "xor",
+    "KeyManager",
+    "WeightManager",
+]
