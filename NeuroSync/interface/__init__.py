47e48b83820193eeb9e8920f65c06d323eb68943
Nikolay Valkanov <niki@valkanovi.com>
2026-01-15 04:06:54 +0200

    UPDATE: Included all important things from the interface and exposed them

---
+++ NeuroSync/interface/__init__.py
@@ -0,0 +1,13 @@
+from NeuroSync.interface.cipher import NeuroSync
+from NeuroSync.interface.sender import Sender
+from NeuroSync.interface.receiver import Receiver
+from NeuroSync.interface.pair import CommunicationPair
+from NeuroSync.interface.visualizer import Visualizer
+
+__all__ = [
+    "NeuroSync",
+    "Sender",
+    "Receiver",
+    "CommunicationPair",
+    "Visualizer",
+]
